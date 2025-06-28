4c0fe10f20e1b79e26e518979e045a7745286467
ArunvijaySNAV <arunvijaysnav@gmail.com>
2025-06-28 13:09:10 +0530

    intial, files uploader.

--- web_camera.py
+++ web_camera.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.title("Auto Grey-scale image")
 st.subheader("Converts original image to grey-scale image")
 
-
+st.write("<b>To access camera:</b>", unsafe_allow_html=True)
 with st.expander("Start camera"):
      cam_img = st.camera_input("Devices web.camera")
      st.write("Camera is active now!")
@@ -14,3 +14,12 @@ if cam_img:
     grey_scale = img.convert("L")
     st.image(grey_scale)
 
+st.write("<b>To access files:</b>", unsafe_allow_html=True)
+with st.expander("Upload image"):
+    img_doc = st.file_uploader("Devices files")
+    st.write("Devices files are active now!")
+
+if img_doc:
+    img = Image.open(img_doc)
+    grey_scale = img.convert("L")
+    st.image(grey_scale)
